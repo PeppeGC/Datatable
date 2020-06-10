1ed4ce0951300163f2ec465bcd7423b4cb4f6add
gianda <giandamarino@gmail.com>
2020-06-10 13:37:40 +0200

    Solved Excel Button Issue
    //Solution for missing Excel Button:
    //https://datatables.net/forums/discussion/49747/excel-button-not-showing-up-using-webpack

--- not work/js/app/showNavbar1.js
+++ not work/js/app/showNavbar1.js
@@ -12,6 +12,10 @@ define([
     "vfs_fonts",
     "buttons.html5.min"
 ], function($) {
+	//Solution for missing Excel Button:
+	//https://datatables.net/forums/discussion/49747/excel-button-not-showing-up-using-webpack
+    window.JSZip = require('jszip.min');
+
 	$(function() {
 	    //TO DO
 	    //$('body').addptodom();
