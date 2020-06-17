8779f17783b84745e4e5d3dec32875fc455c6943
PeppeGC <sgcaprara@libero.it>
2020-06-17 12:31:20 +0200

    quando si fa export xls non deve esportare colonne nascoste https://stackoverflow.com/questions/33972207/datatables-buttons-hide-columns-in-printing-and-exporting-to-excel/40823949
    Cercavo soluzione che non vada definita su ogni button (ma una sola volta)
    
    exportOptions

--- not work/js/lib/custom_leftbar.js
+++ not work/js/lib/custom_leftbar.js
@@ -5,7 +5,7 @@ alert(selectedBar);
 
 
 function dt(){
-		$('#example').DataTable( {
+		table = $('#example').DataTable( {
 			  "scrollY":        "200px",
 			  "scrollCollapse": true,
 			  "paging":         false,
@@ -13,4 +13,26 @@ function dt(){
 			  "dom": "Bfrtip",
 			  "buttons": ["copy", "csv", "excel", "pdf", "print"]
 		} );
+
+	// Get the column number
+	var headers = table.columns().header();
+	var number = table.columns().data().length;
+
+	// Set Toggle Columns
+	$( ".toggle-vis" ).remove();
+	for(var i = 0; i < number; i++){
+		var row = '<a class="toggle-vis" data-column="'+i+'">'+headers[i].innerText+'</a> '
+		$('#toggle-columns').append(row);
+	}
+
+    $('a.toggle-vis').on( 'click', function (e) {
+        e.preventDefault();
+
+        // Get the column API object
+        var column = table.column( $(this).attr('data-column') );
+
+        // Toggle the visibility
+        column.visible( ! column.visible() );
+    } );
+
 }
