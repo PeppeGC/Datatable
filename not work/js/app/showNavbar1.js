aa5e4c93bb4d5f542423eb336a00d56e6e46883c
PeppeGC <sgcaprara@libero.it>
2020-07-21 12:56:23 +0200

    fix #2.1

--- not work/js/app/showNavbar1.js
+++ not work/js/app/showNavbar1.js
@@ -17,21 +17,17 @@ define([
 	    //https://datatables.net/forums/discussion/49747/excel-button-not-showing-up-using-webpack
         window.JSZip = require('jszip.min');
         $(function() {
-	    //TO DO
-	    //$('body').addptodom();
-        console.log('required plugins loading...');
-		dt();
-	    console.log('required plugins loaded...');
+            //TO DO
+            //$('body').addptodom();
+            console.log('required plugins loading...');
+            dt();
+            console.log('required plugins loaded...');
 
- $('.it-date-datepicker').datepicker({
-      inputFormat: ["dd/MM/yyyy"],
-      outputFormat: 'dd/MM/yyyy',
-    });
 
-	    $('.input-daterange input').each(function() {
-        	  //$(this).datepicker('clearDates');
-        	 // alert($(this.value));
-        	  $(this).datepicker();
-          });
+            $('.input-daterange input').each(function() {
+                  //$(this).datepicker('clearDates');
+                 // alert($(this.value));
+                  $(this).datepicker();
+            });
 	    });
 });
